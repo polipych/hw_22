ade0736a5b4f46d1e12f5a53f11aa8e099e2df49
polipych <polipych@yandex.ru>
2023-02-19 13:52:23 +0300

    reverse the logic boolean filter

--- todolist/taskapp/filters.py
+++ todolist/taskapp/filters.py
@@ -6,7 +6,7 @@ class TaskFilterSet(dj_filters.FilterSet):
     """Набор фильров для представления для модели задач."""
 
     title = dj_filters.CharFilter(field_name="title", lookup_expr="icontains")
-    is_active = dj_filters.BooleanFilter(field_name="status")
+    is_active = dj_filters.BooleanFilter(field_name="status", exclude=True)
 
     order_by_field = "ordering"
 
